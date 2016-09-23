e36386a57adb5f0c4d36e09ac3c22c5b06f587f1
dmlch123 <dmlch123@nhn.com>
2016-09-23 16:35:18 +0900

    remove webstorm App directiory

--- musicapp/musicapp/src/test/java/com/nts/gt/appium/unit/PushPrint.java
+++
@@ -1,9 +0,0 @@
-package com.nts.gt.appium.unit;
-
-public class PushPrint {
-	public static void main(String[] args) {
-		System.out.println("Add Java Class File Push");
-		System.out.println("World with 42");
-		System.out.println("World with Miku");
-	}
-}
