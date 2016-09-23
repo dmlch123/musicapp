e65918adb4797dcfc01a850f4e812739a2531d4a
dmlch123 <dmlch123@nhn.com>
2016-09-23 15:05:53 +0900

    PushPrint.java modified on GitHub

--- musicapp/musicapp/src/test/java/com/nts/gt/appium/unit/PushPrint.java
+++ musicapp/musicapp/src/test/java/com/nts/gt/appium/unit/PushPrint.java
@@ -3,5 +3,6 @@ package com.nts.gt.appium.unit;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with 42");
 	}
 }
